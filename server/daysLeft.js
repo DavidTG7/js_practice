5bb214f62371cc4311ffa4cba4b6613ddb50aa93
DavidTG7 <davidthekoa@gmail.com>
2022-09-11 15:54:01 -0500

    Update daysLeft exercise

--- server/daysLeft.js
+++ server/daysLeft.js
@@ -1,46 +1,29 @@
 function daysToXmas(date) {
   // ¡Y no olvides compartir tu solución en redes!
-  const monthsIndex = {
-    Jan: 0,
-    Feb: 1,
-    Mar: 2,
-    Abr: 2,
-    May: 4,
-    Jun: 5,
-    Jul: 6,
-    Aug: 7,
-    Sep: 8,
-    Oct: 9,
-    Nov: 10,
-    Dec: 11,
-  };
   const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  const myDate = date.split(" ");
+  const monthIndex = date.getMonth()
 
-  const monthStr = myDate[0].slice(0, 3);
-
-  let queryMonth = monthsIndex[monthStr];
-
-  let queryDay = Number(myDate[1].replace(",", ""));
-  let queryYear = Number(myDate[2]);
+  let queryDay = date.getDate()
+  let queryYear = date.getFullYear()
+  console.log(queryYear);
 
   let coreDaysLeft = 0;
-  let sameMonthDaysLeft = months[queryMonth] - queryDay;
+  let sameMonthDaysLeft = months[monthIndex] - queryDay;
   let totalDaysLeft = 0;
   let isDone = false;
 
   // sameMonthDaysLeft =
   while (!isDone) {
     if (queryYear < 2021) {
-      for (let i = queryMonth; i < 11; i++) {
+      for (let i = monthIndex; i < 11; i++) {
         coreDaysLeft += months[i];
       }
       queryYear += 1;
-      queryMonth = monthsIndex.Jan;
+      monthIndex = 0;
       coreDaysLeft += 31;
     } else if (queryYear === 2021) {
-      for (let i = queryMonth; i < 11; i++) {
+      for (let i = monthIndex; i < 11; i++) {
         coreDaysLeft += months[i];
       }
       totalDaysLeft = sameMonthDaysLeft + coreDaysLeft - 6;
@@ -49,7 +32,7 @@ function daysToXmas(date) {
 
     if (queryYear > 2021) {
       sameMonthDaysLeft = queryDay;
-      for (let i = queryMonth - 1; i > -1; i--) {
+      for (let i = monthIndex - 1; i > -1; i--) {
         coreDaysLeft += months[i];
       }
       totalDaysLeft = (coreDaysLeft + 6 + sameMonthDaysLeft) * -1;
@@ -60,4 +43,8 @@ function daysToXmas(date) {
   return totalDaysLeft;
 }
 
-console.log(daysToXmas("Feb 1, 2022"));
+const myDate = new Date("1/1/22")
+
+console.log(myDate);
+
+console.log(daysToXmas(myDate));
