7a39364a448ad3ed83ab3d4a8fda8b3bde7189a7
DavidTG7 <davidthekoa@gmail.com>
2022-09-11 23:22:45 -0500

    flatMap and join methods exercises

--- server/11-join.js
+++ server/11-join.js
@@ -5,3 +5,16 @@ console.log(elements.join(''));
 const title = 'Arrays manipulation course'
 
 console.log(title.split(' ').join('-').toLowerCase());
+
+
+function solution(lines) {
+  // Tu código aquí 👈 
+  return lines.flatMap(item => item.split(' ')).length
+}; 
+
+console.log(solution([
+  "Beautiful is better than ugly",
+  "Explicit is better than implicit",
+  "Simple is better than complex",
+  "Complex is better than complicated",
+]));
