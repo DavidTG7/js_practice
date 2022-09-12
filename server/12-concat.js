bcb0e79f8e661ec27d88df0ad71bf4416eb4b57f
DavidTG7 <davidthekoa@gmail.com>
2022-09-11 19:10:48 -0500

    Concat method exercise

---
+++ server/12-concat.js
@@ -0,0 +1,8 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
+const elements = [1, 2, 3, 4, 5]
+
+const newArr = animals.concat(elements)
+
+console.log(newArr);
+
+console.log('Hello ', 'World');
